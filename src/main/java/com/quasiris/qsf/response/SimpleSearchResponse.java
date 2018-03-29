16267ac94de14fcaadc330b6ba5b708f2f28696b
Marius Kolkowski <marius.kolkowski@quasiris.de>
2018-03-29 14:33:31 +0200

    implement a simple search response

--- src/main/java/com/quasiris/qsf/response/SimpleSearchResponse.java
+++ src/main/java/com/quasiris/qsf/response/SimpleSearchResponse.java
@@ -2,14 +2,28 @@ package com.quasiris.qsf.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
  * Created by mki on 03.12.17.
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class SimpleSearchResponse extends HashMap<String,Map<String, Object>> {
+public class SimpleSearchResponse extends ArrayList<Map<String, Object>> {
 
 
+    public static SimpleSearchResponse create(SearchResult searchResult) {
+        SimpleSearchResponse searchResponse = new SimpleSearchResponse();
+        if(searchResult == null) {
+            return searchResponse;
+        }
+        if(searchResult.getDocuments() == null) {
+            return searchResponse;
+        }
+
+        for(Document document : searchResult.getDocuments()) {
+            searchResponse.add(document.getDocument());
+        }
+        return searchResponse;
+    }
 }
