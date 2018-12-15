7e4b44b404a9690b3a3adadcfc905c73c76877c9
Thomas Bärwinkel <tbl@gmx.de>
2018-12-15 21:36:31 +0100

    implement the QSFHttpServletRequest

---
+++ src/main/java/com/quasiris/qsf/pipeline/filter/web/QSFHttpServletRequest.java
@@ -0,0 +1,428 @@
+package com.quasiris.qsf.pipeline.filter.web;
+
+import com.google.common.base.Splitter;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLDecoder;
+import java.security.Principal;
+import java.util.*;
+
+public class QSFHttpServletRequest implements HttpServletRequest {
+
+
+    private String url;
+
+    private URI uri;
+
+    private Enumeration<String> parameterNames;
+
+    private Map<String, List<String>> parametersMap;
+
+    private Map<String, String[]> decodedParametersMap;
+
+    public QSFHttpServletRequest(String url) throws URISyntaxException {
+        this.url = url;
+        init();
+    }
+
+    private void init() throws URISyntaxException {
+
+        uri = new URI(url);
+        final Iterable<String> parameterList = Splitter.on("&")
+                .omitEmptyStrings()
+                .trimResults().split(uri.getQuery());
+
+        parametersMap = new HashMap<>();
+
+        decodedParametersMap = new HashMap<>();
+
+        for (String param : parameterList) {
+            List<String> splitted = Splitter.on("=").splitToList(param);
+            if (splitted.size() != 2) {
+                throw new URISyntaxException(url, "The url " + url + " is not valid for param: " + param);
+            }
+            String key = splitted.get(0);
+            List<String> values = parametersMap.get(key);
+            if (values == null) {
+                values = new ArrayList<>();
+            }
+            values.add(splitted.get(1));
+            parametersMap.put(key, values);
+        }
+
+        for (Map.Entry<String, List<String>> entry : parametersMap.entrySet()) {
+            List<String> decodedValues = new ArrayList<>();
+            for (String value : entry.getValue()) {
+                decodedValues.add(decodeUrlParam(value));
+            }
+            decodedParametersMap.put(entry.getKey(), decodedValues.toArray(new String[0]));
+
+        }
+        parameterNames = Collections.enumeration(parametersMap.keySet());
+    }
+
+
+    public String decodeUrlParam(String param) throws URISyntaxException {
+        try {
+            return URLDecoder.decode(param, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new URISyntaxException(url, "The param " + param + " could not be decoded.");
+        }
+    }
+
+    @Override
+    public String getAuthType() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public Cookie[] getCookies() {
+        Cookie[] cookies = {};
+        return cookies;
+    }
+
+    @Override
+    public long getDateHeader(String s) {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getHeader(String s) {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public Enumeration<String> getHeaders(String s) {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public Enumeration<String> getHeaderNames() {
+        return Collections.emptyEnumeration();
+    }
+
+    @Override
+    public int getIntHeader(String s) {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getMethod() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getPathInfo() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getPathTranslated() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getContextPath() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getQueryString() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getRemoteUser() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public boolean isUserInRole(String s) {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public Principal getUserPrincipal() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getRequestedSessionId() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getRequestURI() {
+        return uri.getPath();
+    }
+
+    @Override
+    public StringBuffer getRequestURL() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getServletPath() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public HttpSession getSession(boolean b) {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public HttpSession getSession() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String changeSessionId() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public boolean isRequestedSessionIdValid() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public boolean isRequestedSessionIdFromCookie() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public boolean isRequestedSessionIdFromURL() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public boolean isRequestedSessionIdFromUrl() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public boolean authenticate(HttpServletResponse httpServletResponse) throws IOException, ServletException {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public void login(String s, String s1) throws ServletException {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public void logout() throws ServletException {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public Collection<Part> getParts() throws IOException, ServletException {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public Part getPart(String s) throws IOException, ServletException {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public <T extends HttpUpgradeHandler> T upgrade(Class<T> aClass) throws IOException, ServletException {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public Object getAttribute(String s) {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public Enumeration<String> getAttributeNames() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getCharacterEncoding() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public void setCharacterEncoding(String s) throws UnsupportedEncodingException {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public int getContentLength() {
+       throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public long getContentLengthLong() {
+       throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getContentType() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getParameter(String key) {
+        List<String> values = parametersMap.get(key);
+        if(values == null || values.size() == 0) {
+            throw new UnsupportedOperationException("The method is not implemented yet.");
+        }
+        return values.get(0);
+    }
+
+    @Override
+    public Enumeration<String> getParameterNames() {
+        return parameterNames;
+    }
+
+    @Override
+    public String[] getParameterValues(String s) {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public Map<String, String[]> getParameterMap() {
+        return decodedParametersMap;
+    }
+
+    @Override
+    public String getProtocol() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getScheme() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getServerName() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public int getServerPort() {
+       throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getRemoteAddr() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getRemoteHost() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public void setAttribute(String s, Object o) {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public void removeAttribute(String s) {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public Locale getLocale() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public Enumeration<Locale> getLocales() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public boolean isSecure() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public RequestDispatcher getRequestDispatcher(String s) {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getRealPath(String s) {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public int getRemotePort() {
+       throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getLocalName() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public String getLocalAddr() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public int getLocalPort() {
+       throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public ServletContext getServletContext() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public AsyncContext startAsync() throws IllegalStateException {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public AsyncContext startAsync(ServletRequest servletRequest, ServletResponse servletResponse) throws IllegalStateException {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public boolean isAsyncStarted() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public boolean isAsyncSupported() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public AsyncContext getAsyncContext() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    @Override
+    public DispatcherType getDispatcherType() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+}
--- src/test/java/com/quasiris/qsf/mock/Mockfactory.java
+++ src/test/java/com/quasiris/qsf/mock/Mockfactory.java
@@ -1,8 +1,8 @@
 package com.quasiris.qsf.mock;
 
-import com.google.common.base.Splitter;
 import com.google.common.io.Files;
 import com.quasiris.qsf.pipeline.filter.solr.MockSolrClient;
+import com.quasiris.qsf.pipeline.filter.web.QSFHttpServletRequest;
 import org.apache.http.HttpEntity;
 import org.apache.http.StatusLine;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -10,15 +10,11 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.mockito.Mockito;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URLDecoder;
+import java.net.URISyntaxException;
 import java.nio.charset.Charset;
-import java.util.*;
 
 /**
  * Created by mki on 25.11.17.
@@ -47,72 +43,9 @@ public class Mockfactory {
 
     public static HttpServletRequest createHttpServletRequest(String url){
         try {
-            URI uri = new URI(url);
-
-
-            final Iterable<String> parameterList = Splitter.on("&")
-                    .omitEmptyStrings()
-                    .trimResults().split(uri.getQuery());
-
-            final Map<String, List<String>> parametersMap = new HashMap<>();
-
-            final Map<String, String[]> decodedParametersMap = new HashMap<>();
-
-            for(String param : parameterList) {
-                List<String> splitted = Splitter.on("=").splitToList(param);
-                if(splitted.size() != 2) {
-                    throw new IllegalArgumentException("The url " + url + " is not valid for param: " + param);
-                }
-                String key = splitted.get(0);
-                List<String> values = parametersMap.get(key);
-                if(values == null) {
-                    values = new ArrayList<>();
-                }
-                values.add(splitted.get(1));
-                parametersMap.put(key, values);
-            }
-
-            for(Map.Entry<String, List<String>> entry : parametersMap.entrySet()) {
-                List<String> decodedValues = new ArrayList<>();
-                for(String value: entry.getValue()) {
-                    decodedValues.add(decodeUrlParam(value));
-                }
-                decodedParametersMap.put(entry.getKey(), decodedValues.toArray(new String[0]));
-
-            }
-
-
-
-
-
-
-            HttpServletRequest httpServletRequest = Mockito.mock(HttpServletRequest.class);
-            Enumeration<String> parameterNames = Collections.enumeration(parametersMap.keySet());
-            Mockito.when(httpServletRequest.getParameterNames()).thenReturn(parameterNames);
-            for (String key : parametersMap.keySet()) {
-                Mockito.when(httpServletRequest.getParameter(key)).thenReturn(decodedParametersMap.get(key)[0]);
-            }
-
-            Mockito.when(httpServletRequest.getHeaderNames()).thenReturn(parameterNames);
-            Cookie[] cookies = {};
-            Mockito.when(httpServletRequest.getCookies()).thenReturn(cookies);
-
-            Mockito.when(httpServletRequest.getRequestURI()).thenReturn(uri.getPath());
-
-            Mockito.when(httpServletRequest.getParameterMap()).thenReturn(decodedParametersMap);
-
-            return httpServletRequest;
-
-        } catch (Exception e ) {
+            return new QSFHttpServletRequest(url);
+        } catch (URISyntaxException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    public static String decodeUrlParam(String param) {
-        try {
-            return URLDecoder.decode(param, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            return param;
         }
     }
 
