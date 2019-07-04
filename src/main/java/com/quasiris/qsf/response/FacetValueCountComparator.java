80b08cb8ba21efe2fd1b6108526fe70a45765d80
Thomas Bärwinkel <tbl@gmx.de>
2019-07-04 21:26:56 +0200

    allow to set the sort direction in the facet value count comparator

--- src/main/java/com/quasiris/qsf/response/FacetValueCountComparator.java
+++ src/main/java/com/quasiris/qsf/response/FacetValueCountComparator.java
@@ -6,11 +6,25 @@ import java.util.Comparator;
  * Created by mki on 31.12.17.
  */
 public class FacetValueCountComparator implements Comparator<FacetValue> {
+
+    private boolean ascending = true;
+
+    public FacetValueCountComparator() {
+    }
+
+    public FacetValueCountComparator(boolean ascending) {
+        this.ascending = ascending;
+    }
+
     @Override
     public int compare(FacetValue v1, FacetValue v2) {
         if(v1 == null || v2 == null) {
             return 0;
         }
-        return v1.getCount().compareTo(v2.getCount());
+        if(ascending) {
+            return v1.getCount().compareTo(v2.getCount());
+        } else {
+            return v2.getCount().compareTo(v1.getCount());
+        }
     }
 }
