7afd66284177ccf98d23b095b87b6f1557225cdc
Thomas Bärwinkel <tbl@gmx.de>
2019-09-18 20:31:40 +0200

    add a date util method that supports microseconds

--- src/main/java/com/quasiris/qsf/util/DateUtil.java
+++ src/main/java/com/quasiris/qsf/util/DateUtil.java
@@ -1,13 +1,22 @@
 package com.quasiris.qsf.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 /**
  * A util for converting dates.
  */
 public class DateUtil {
+
+
+    public static final String ELASTIC_DATE_PATTERN = "yyyy-MM-dd'T'HH:mm:ssZ";
+
+    // 2019-09-16T20:08:28.275+0000
+    public static final String ELASTIC_DATE_PATTERN_MICROSECONDS = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
 
     /**
      * Creates a instant by a date string.
@@ -25,7 +34,20 @@ public class DateUtil {
      * @return the created instant.
      */
     public static Instant getInstantByElasticDate(String date) {
-        OffsetDateTime ld = OffsetDateTime.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ"));
+        OffsetDateTime ld = OffsetDateTime.parse(date, DateTimeFormatter.ofPattern(ELASTIC_DATE_PATTERN));
         return ld.toInstant();
+    }
+
+    public static Date getDateByElasticDateMicroseconds(String date) throws ParseException {
+        return getDateByPattern(date, ELASTIC_DATE_PATTERN_MICROSECONDS);
+    }
+
+    public static Date getDateByElasticDate(String date) throws ParseException {
+        return getDateByPattern(date, ELASTIC_DATE_PATTERN);
+    }
+
+    public static Date getDateByPattern(String date, String pattern) throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat(pattern);
+        return formatter.parse(date);
     }
 }
