fdfee82565682fd34051c49908c4da0a2bbbe135
Thomas Bärwinkel <tbl@gmx.de>
2021-08-29 01:14:40 +0200

    implement a DateFormatFacetKeyMapper

---
+++ src/main/java/com/quasiris/qsf/pipeline/filter/mapper/DateFormatFacetKeyMapper.java
@@ -0,0 +1,31 @@
+package com.quasiris.qsf.pipeline.filter.mapper;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class DateFormatFacetKeyMapper implements FacetKeyMapper {
+
+    private DateTimeFormatter formatter;
+
+    private ZoneId zoneId;
+
+    public DateFormatFacetKeyMapper(String pattern) {
+        this.formatter = DateTimeFormatter.ofPattern(pattern);
+        this.zoneId = ZoneId.of("Europe/Berlin");
+    }
+
+    public DateFormatFacetKeyMapper(String pattern, String zoneId) {
+        this.formatter = DateTimeFormatter.ofPattern(pattern);
+        this.zoneId = ZoneId.of(zoneId);
+    }
+
+    @Override
+    public String map(String value) {
+
+        final String formattedDate = Instant.ofEpochMilli(Long.valueOf(value))
+                .atZone(zoneId)
+                .format(formatter);
+        return formattedDate;
+    }
+}
---
+++ src/test/java/com/quasiris/qsf/pipeline/filter/mapper/DateFormatFacetKeyMapperTest.java
@@ -0,0 +1,18 @@
+package com.quasiris.qsf.pipeline.filter.mapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateFormatFacetKeyMapperTest {
+
+
+    @Test
+    public void test2016() {
+
+        DateFormatFacetKeyMapper mapper = new DateFormatFacetKeyMapper("yyyy");
+        String actual = mapper.map("1451602800000");
+        assertEquals("2016", actual);
+    }
+
+}
