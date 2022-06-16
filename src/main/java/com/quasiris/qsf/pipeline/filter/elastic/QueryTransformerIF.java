edc7edf638039321421a2038da111f01c55e99fb
Thomas Bärwinkel <tbl@quasiris.de>
2022-06-16 12:36:14 +0200

    make it possible to stop a pipeline in a query transformer

--- src/main/java/com/quasiris/qsf/pipeline/filter/elastic/QueryTransformerIF.java
+++ src/main/java/com/quasiris/qsf/pipeline/filter/elastic/QueryTransformerIF.java
@@ -3,6 +3,7 @@ package com.quasiris.qsf.pipeline.filter.elastic;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.quasiris.qsf.pipeline.PipelineContainer;
 import com.quasiris.qsf.pipeline.PipelineContainerException;
+import com.quasiris.qsf.pipeline.exception.PipelineStopException;
 
 /**
  * Created by mki on 04.92.18.
@@ -10,7 +11,7 @@ import com.quasiris.qsf.pipeline.PipelineContainerException;
 public interface QueryTransformerIF {
 
 
-    ObjectNode transform(PipelineContainer pipelineContainer) throws PipelineContainerException;
+    ObjectNode transform(PipelineContainer pipelineContainer) throws PipelineContainerException, PipelineStopException;
 
     StringBuilder print(String indent);
 }
