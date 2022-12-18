542bab6d04c7251bd489d688feb0bc0d0023447b
Thomas Bärwinkel <tbl@quasiris.de>
2022-12-18 22:23:46 +0100

    print arrays line by line

--- src/main/java/com/quasiris/qsf/test/json/JsonAssert.java
+++ src/main/java/com/quasiris/qsf/test/json/JsonAssert.java
@@ -1,7 +1,10 @@
 package com.quasiris.qsf.test.json;
 
+import com.fasterxml.jackson.core.util.DefaultIndenter;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.quasiris.qsf.commons.util.IOUtils;
 
 import java.io.IOException;
@@ -19,10 +22,15 @@ public class JsonAssert {
 
     public static void assertJson(String expected, Object json) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        String query = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+        DefaultPrettyPrinter prettyPrinter = new DefaultPrettyPrinter();
+        prettyPrinter.indentArraysWith(DefaultIndenter.SYSTEM_LINEFEED_INSTANCE);
+
+        String query = objectMapper.writer(prettyPrinter).writeValueAsString(json);
 
         JsonNode expectedJson = objectMapper.readValue(expected, JsonNode.class);
-        expected = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(expectedJson);
+        expected = objectMapper.writer(prettyPrinter).writeValueAsString(expectedJson);
         assertEquals(expected, query);
     }
 }
