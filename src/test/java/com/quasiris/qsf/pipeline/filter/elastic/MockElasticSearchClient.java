bcc5c5744052c42875ec8cf93184c40a996ad7c3
Jan Bajaryn <jbn@quasiris.de>
2023-08-22 16:01:02 +0200

    fix tests

--- src/test/java/com/quasiris/qsf/pipeline/filter/elastic/MockElasticSearchClient.java
+++ src/test/java/com/quasiris/qsf/pipeline/filter/elastic/MockElasticSearchClient.java
@@ -6,6 +6,7 @@ import com.quasiris.qsf.pipeline.filter.elastic.bean.ElasticResult;
 import com.quasiris.qsf.pipeline.filter.elastic.bean.MultiElasticResult;
 import com.quasiris.qsf.pipeline.filter.elastic.client.StandardMultiElasticClient;
 
+import java.time.Duration;
 import java.util.List;
 
 
@@ -20,6 +21,20 @@ public class MockElasticSearchClient extends ElasticSearchClient {
 
     @Override
     public ElasticResult search(String indexUrl, String jsonQuery) throws ResourceNotFoundException {
+        MockRequestFileHandler mockRequestFileHandler = new MockRequestFileHandler(mockDir, mockFile);
+        if(mock) {
+            return mockRequestFileHandler.getMockedElasticResult(jsonQuery, ElasticResult.class);
+        }
+
+        ElasticResult elasticResult = super.search(indexUrl, jsonQuery);
+        if(record) {
+            mockRequestFileHandler.recordQueryElasticResult(jsonQuery, elasticResult);
+        }
+        return elasticResult;
+    }
+
+    @Override
+    public ElasticResult search(String indexUrl, String jsonQuery, Duration requestTimeout) throws ResourceNotFoundException {
         MockRequestFileHandler mockRequestFileHandler = new MockRequestFileHandler(mockDir, mockFile);
         if(mock) {
             return mockRequestFileHandler.getMockedElasticResult(jsonQuery, ElasticResult.class);
